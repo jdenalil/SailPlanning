ef02c53b2c3dc67b4eba31185f466528a331e690
Jason Lang <jason.lang@shield.ai>
2023-10-22 11:02:17 -0400

    formatting

--- src/astar/mod.rs
+++ src/astar/mod.rs
@@ -1,7 +1,6 @@
-use pathfinding::prelude::astar;
-use eqsolver::single_variable::FDNewton;
 use crate::dynamics::energy_use;
-
+use eqsolver::single_variable::FDNewton;
+use pathfinding::prelude::astar;
 
 // PosTime struct for nodes
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -31,32 +30,38 @@ pub fn run_astar(goal: PosTime, current: Current, max_speed: f64) -> Option<(Vec
     )
 }
 
-
-fn find_successors(point: &PosTime, goal: &PosTime, current: Current, max_speed: f64) -> Vec<(PosTime, u32)> {
+fn find_successors(
+    point: &PosTime,
+    goal: &PosTime,
+    current: Current,
+    max_speed: f64,
+) -> Vec<(PosTime, u32)> {
     // if we're at the end location but haven't hit the goal time, hold at location
     if point.0 == goal.0 && point.1 == goal.1 && point.2 < goal.2 {
         // if so, calculate time remaining and energy needed to sail against current until the goal time
         // Return a single element with goal position, goal time, current speed, and power needed
-        return vec![
-            (PosTime(goal.0, goal.1, goal.2), 
-            calc_hold_power(goal.2, point.2, 0, current.magnitude)),
-        ];
+        return vec![(
+            PosTime(goal.0, goal.1, goal.2),
+            calc_hold_power(goal.2, point.2, 0, current.magnitude),
+        )];
     }
 
     // generate all possible movements
     let points: Vec<PosTime> = vec![
-        PosTime(point.0 + 1, point.1, goal.2), 
-        PosTime(point.0 - 1, point.1, goal.2), 
-        PosTime(point.0, point.1 + 1, goal.2), 
-        PosTime(point.0, point.1 - 1, goal.2), 
-        PosTime(point.0 + 1, point.1 + 1, goal.2), 
-        PosTime(point.0 + 1, point.1 - 1, goal.2), 
-        PosTime(point.0 - 1, point.1 + 1, goal.2), 
+        PosTime(point.0 + 1, point.1, goal.2),
+        PosTime(point.0 - 1, point.1, goal.2),
+        PosTime(point.0, point.1 + 1, goal.2),
+        PosTime(point.0, point.1 - 1, goal.2),
+        PosTime(point.0 + 1, point.1 + 1, goal.2),
+        PosTime(point.0 + 1, point.1 - 1, goal.2),
+        PosTime(point.0 - 1, point.1 + 1, goal.2),
         PosTime(point.0 - 1, point.1 - 1, goal.2),
     ];
 
     // Generate NUM_SPEEDS_TO_SEARCH possible speeds in the range [0.0, max_speed]
-    let speeds: Vec<f64> = (0..=NUM_SPEEDS_TO_SEARCH).map(|i| (i as f64) * max_speed / NUM_SPEEDS_TO_SEARCH as f64).collect();
+    let speeds: Vec<f64> = (0..=NUM_SPEEDS_TO_SEARCH)
+        .map(|i| (i as f64) * max_speed / NUM_SPEEDS_TO_SEARCH as f64)
+        .collect();
 
     // Now, for each point, let's try each speed, filtering unreachable point / speed combos
     points
@@ -64,18 +69,27 @@ fn find_successors(point: &PosTime, goal: &PosTime, current: Current, max_speed:
         .flat_map(|next_point| {
             speeds.iter().filter_map(move |&speed| {
                 calculate_traversal_time(point, &next_point, current, speed).map(|addl_time| {
-                    (PosTime(next_point.0, next_point.1, point.2 + addl_time), calc_traversal_power(addl_time, speed))
+                    (
+                        PosTime(next_point.0, next_point.1, point.2 + addl_time),
+                        calc_traversal_power(addl_time, speed),
+                    )
                 })
             })
-        }).collect()
+        })
+        .collect()
 }
 
-
-fn calc_power_hueristic(point: &PosTime, goal: &PosTime, current: Current, boat_water_speed: f64) -> u32 {
+fn calc_power_hueristic(
+    point: &PosTime,
+    goal: &PosTime,
+    current: Current,
+    boat_water_speed: f64,
+) -> u32 {
     match calculate_traversal_time(point, goal, current, boat_water_speed) {
         Some(traversal_time) => {
             // return traversal power + hold power
-            calc_traversal_power(traversal_time, boat_water_speed) + calc_hold_power(goal.2, point.2, traversal_time, current.magnitude)
+            calc_traversal_power(traversal_time, boat_water_speed)
+                + calc_hold_power(goal.2, point.2, traversal_time, current.magnitude)
         }
         None => {
             // return high energy value - don't search here!
@@ -84,8 +98,12 @@ fn calc_power_hueristic(point: &PosTime, goal: &PosTime, current: Current, boat_
     }
 }
 
-
-fn calculate_traversal_time(point: &PosTime, goal: &PosTime, current: Current, boat_water_speed: f64) -> Option<u32> {
+fn calculate_traversal_time(
+    point: &PosTime,
+    goal: &PosTime,
+    current: Current,
+    boat_water_speed: f64,
+) -> Option<u32> {
     // first calculate the distance and direction to the goal point
     let x_distance: i32 = goal.0 - point.0;
     let y_distance: i32 = goal.1 - point.1;
@@ -94,8 +112,10 @@ fn calculate_traversal_time(point: &PosTime, goal: &PosTime, current: Current, b
 
     // set up equation for time to get to local goal point
     let time_equation = |time_elapsed: f64| {
-        let x: f64 = (goal_distance * goal_direction.sin()) - (time_elapsed * current.magnitude * current.direction.sin());
-        let y: f64 = (goal_distance * goal_direction.cos()) - (time_elapsed * current.magnitude * current.direction.cos());
+        let x: f64 = (goal_distance * goal_direction.sin())
+            - (time_elapsed * current.magnitude * current.direction.sin());
+        let y: f64 = (goal_distance * goal_direction.cos())
+            - (time_elapsed * current.magnitude * current.direction.cos());
         ((x.powi(2) + y.powi(2)).sqrt() / boat_water_speed) - time_elapsed
     };
 
@@ -118,18 +138,20 @@ fn calculate_traversal_time(point: &PosTime, goal: &PosTime, current: Current, b
     }
 }
 
-
-fn calc_hold_power(goal_time: u32, point_time: u32, traversal_time: u32, current_speed: f64) -> u32 {
+fn calc_hold_power(
+    goal_time: u32,
+    point_time: u32,
+    traversal_time: u32,
+    current_speed: f64,
+) -> u32 {
     ((goal_time - point_time - traversal_time) as f64 * energy_use(current_speed)) as u32
 }
 
-
 fn calc_traversal_power(traversal_time: u32, traversal_speed: f64) -> u32 {
-    (traversal_time as f64 * energy_use(traversal_speed)) as u32 
+    (traversal_time as f64 * energy_use(traversal_speed)) as u32
 }
 
-
-/* 
+/*
 #[cfg(test)]
 mod tests {
     use super::calc_power;
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 mod astar;
 mod dynamics;
 
-use crate::astar::{PosTime, Current, run_astar, SCALING_FACTOR};
+use crate::astar::{run_astar, Current, PosTime, SCALING_FACTOR};
 
 fn main() {
-  // we're given a relative goal point, time of arrival, ocean current, and max_speed
-  let goal: PosTime = PosTime(15, 20, 10 * SCALING_FACTOR as u32); // goal location is relative to origin
-  let current = Current {magnitude: 1.0, direction: 0.7}; // direction measured from north
-  let max_speed = 10.0;
-  // run optimizations
-  let result = run_astar(goal, current, max_speed);
-  match result {
-    Some(t) => {
-      for p in t.0 {
-        println!("x {} y {} time {}", p.0, p.1, p.2 as f64 / SCALING_FACTOR);
-      }
-      println!("found power cost {}", t.1 as f64 / SCALING_FACTOR);
+    // we're given a relative goal point, time of arrival, ocean current, and max_speed
+    let goal: PosTime = PosTime(15, 20, 10 * SCALING_FACTOR as u32); // goal location is relative to origin
+    let current = Current {
+        magnitude: 1.0,
+        direction: 0.7,
+    }; // direction measured from north
+    let max_speed = 10.0;
+    // run optimizations
+    let result = run_astar(goal, current, max_speed);
+    match result {
+        Some(t) => {
+            for p in t.0 {
+                println!("x {} y {} time {}", p.0, p.1, p.2 as f64 / SCALING_FACTOR);
+            }
+            println!("found power cost {}", t.1 as f64 / SCALING_FACTOR);
+        }
+        None => {}
     }
-    None => {}
-  }
 }
